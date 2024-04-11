0a6645283cdc63312ebbbd464b1a772980b43c0d
ADDosSantos <57597865+ADDosSantos@users.noreply.github.com>
2024-04-11 18:59:26 +0100

    having fun with multi browser and headless

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ Python + Selenium 4 exercising Page Object Model
 
 
 using Faker for random test data.
+
+usage example
+
+(default)
+python -m pytest --browser chrome .\tests\test_01_login.py 
+
+python -m pytest --remote --browser firefox .\tests\test_01_login.py
+
+python -m pytest --remote --browser --headless chrome .\tests\test_01_login.py
+
--- conftest.py
+++ conftest.py
@@ -1,12 +1,10 @@
-#testing 1234
-
 import pytest
 import time
 import requests
 import os
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -20,23 +18,67 @@ videos_folder = "videos"
 if not os.path.exists(videos_folder):
     os.makedirs(videos_folder)
 
+def pytest_addoption(parser):
+    parser.addoption("--remote", action="store_true", help="Run tests remotely using Selenoid")
+    parser.addoption("--headless", action="store_true", help="Specify headless execution for testing")
+    parser.addoption("--browser", action="store", default="chrome", help="Specify the browser for testing")
+    
 @pytest.fixture(scope="class")
-def browser():
-    # Define desired capabilities for Chrome browser
-    chrome_capabilities = DesiredCapabilities.CHROME.copy()
-    chrome_capabilities["browserName"] = "chrome"
-    options = ChromeOptions()
-    selenoid_options = {"enableVideo": True}
-    options.set_capability("selenoid:options", selenoid_options)
+def browser(request):
+    # Check if --local parameter is passed
+    use_remote = request.config.getoption("--remote")
+    # Check if --browser parameter is passed
+    browser_type = request.config.getoption("--browser")
+    # Check if --browser parameter is passed
+    headless = request.config.getoption("--headless")
+
+        # Define desired capabilities
+    capabilities = {
+        "chrome": DesiredCapabilities.CHROME.copy(),
+        "firefox": DesiredCapabilities.FIREFOX.copy(),
+    }
+    options = {
+        "chrome": ChromeOptions(),
+        "firefox": FirefoxOptions(),
+    }
+
+    if use_remote:
+        # Set Selenoid options for video recording
+        selenoid_options = {"enableVideo": True}
+        if headless and browser_type.lowercase()=="chrome":
+            selenoid_options.update({"headless": True})
+        if headless and browser_type.lowercase()=="firefox":
+            selenoid_options.update({"moz:headless": True})
+
+        options[browser_type].set_capability("selenoid:options", selenoid_options)
+        driver = webdriver.Remote(selenoid_url, desired_capabilities=capabilities[browser_type], options=options[browser_type]) ###SERVER DOWN!!!
     
-# Initialize WebDriver with Selenoid
-    driver = webdriver.Remote(
-        selenoid_url, options=options
-    )
-    driver.get("https://www.saucedemo.com")
-    driver.set_window_size(1280, 1024)
+    if not use_remote:
+        # Define options for Chrome and Firefox
+        options = {
+            "chrome": ChromeOptions(),
+            "firefox": FirefoxOptions(),
+        }
+        if browser_type == "chrome":
+            # Set any desired capabilities for Chrome
+            options[browser_type].add_argument("--window-size=1280,1024")  # Example of adding a Chrome option
+            # Initialize local Chrome WebDriver with options
+            driver = webdriver.Chrome(options=options[browser_type])
+        elif browser_type == "firefox":
+            # Set any desired capabilities for Firefox
+            options[browser_type].add_argument("--width=1280")
+            options[browser_type].add_argument("--height=1024")
     
-    # initializing implicit wait
+            # Initialize local Firefox WebDriver with options
+            driver = webdriver.Firefox(options=options[browser_type])
+        else:
+            raise ValueError("Invalid browser type specified.")
+    
+        # I was having fun... why not?
+        if headless:
+            options[browser_type].add_argument("--headless")
+    
+    # Initializing implicit wait
     driver.implicitly_wait(7) 
     
     yield driver
